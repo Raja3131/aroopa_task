31ce6af7c2977379830b5d69576d87817641bf5c
Raja <kingraja673@gmail.com>
2022-04-21 09:37:29 +0530

    question 2 is completed

---
+++ src/components/AutoCorrectTextArea.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react';
+
+const AutoCorrectTextArea = () => {
+    const [text, setText] = useState("");
+
+  return (
+    <div className="App">
+    <textarea
+      value={text}
+      onChange={(e) => {
+        setText(e.target.value);
+        const strArr = text.split(" ");
+        const obj = {
+            'realy': 'really',
+            'wierd': 'weird',
+        };
+        const objArr = Object.getOwnPropertyNames(obj);
+        strArr.map((d, i) => {
+          const n = objArr.indexOf(d);
+          if (n !== -1) {
+            strArr[i] = obj[objArr[n]];
+            setText(strArr.join(" ")+" ");
+          }
+          return true;
+        });
+      }}
+    />
+  </div>
+  )
+}
+
+export default AutoCorrectTextArea
